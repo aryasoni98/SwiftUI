dbcf83e37dcdb787f083321d46f9bd24c2a2e1f5
aryasoni98 <aryasoni98@gmail.com>
2021-06-09 21:13:44 +0530

    Update

---
+++ Animating Views and Transitions/Animating Views and Transitions/Animating_Views_and_TransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animating_Views_and_TransitionsApp.swift
+//  Animating Views and Transitions
+//
+//  Created by Arya Soni on 09/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Animating_Views_and_TransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
